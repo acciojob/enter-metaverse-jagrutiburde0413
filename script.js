6e4bae11409d51d67a4de5bb17b1e24ecb79e971
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-05-20 20:27:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+  const statusParagraph = document.getElementById('status');
+    const enterButton = document.getElementById('enterBtn');
+
+    // Add a click event listener to the button
+    enterButton.addEventListener('click', function() {
+      // Update the text content of the paragraph
+      statusParagraph.textContent = 'Entered Metaverse';
+
+      // Create a new h1 element
+      const heading = document.createElement('h1');
+      heading.textContent = statusParagraph.textContent;
+
+      // Replace the paragraph with the new h1 element
+      statusParagraph.parentNode.replaceChild(heading, statusParagraph);
+    });
