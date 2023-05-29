e0d4df38ef5e08a3c2d5370a28fa457b5c9ea1eb
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-05-29 19:44:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ document.addEventListener('DOMContentLoaded', function() {
     statusElement.parentNode.replaceChild(h1, statusElement);
   });
 });
+describe('Metaverse Test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('updates status text on button click', () => {
+    cy.get('#status').should('have.text', 'Enter the Metaverse');
+    cy.get('#enterBtn').click();
+    cy.get('#status').should('not.exist');
+  });
+
+  it('updates status text to heading on button click', () => {
+    cy.get('#enterBtn').click();
+    cy.get('h1').should('have.text', 'Entered Metaverse');
+  });
+});
